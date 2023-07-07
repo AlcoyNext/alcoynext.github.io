43bc36f9ccb618f1970640892dc7456405451787
Cristian Mazzetti Martin <cristian@kalexgroup.com>
2023-07-07 11:11:45 +0200

    add mapbox

---
+++ main.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', function () {
+
+    // DEVELOPMENT
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYWxjb3luZXh0IiwiYSI6ImNsanNhaWg5OTA0cnQzZnA5bjA2b2ZkNHYifQ.MWQzPqXVPqt3Kj0LxIWBdQ';
+    // PRODUCTION
+    //mapboxgl.accessToken = 'pk.eyJ1IjoiYWxjb3luZXh0IiwiYSI6ImNsanNiaHdhejA4eDMzZm4weGhqYTRnaHcifQ.37crDNKv4geComo0kCPQag';
+
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [-0.47298, 38.69826],
+        zoom: 13
+    });
+
+    map.addControl(new mapboxgl.NavigationControl());
+
+})
+
