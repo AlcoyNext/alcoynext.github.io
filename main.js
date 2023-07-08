174f45377fcb8e3acf225e5b2c3b6abbd501cfed
Cristian Mazzetti Martin <cristian@kalexgroup.com>
2023-07-08 07:47:14 +0200

    update css

--- main.js
+++ main.js
@@ -1,32 +1,30 @@
-window.addEventListener('load', function () {
 
-    // DEVELOPMENT
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYWxjb3luZXh0IiwiYSI6ImNsanNhaWg5OTA0cnQzZnA5bjA2b2ZkNHYifQ.MWQzPqXVPqt3Kj0LxIWBdQ';
-    // PRODUCTION
-    //mapboxgl.accessToken = 'pk.eyJ1IjoiYWxjb3luZXh0IiwiYSI6ImNsanNiaHdhejA4eDMzZm4weGhqYTRnaHcifQ.37crDNKv4geComo0kCPQag';
+// DEVELOPMENT
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWxjb3luZXh0IiwiYSI6ImNsanNhaWg5OTA0cnQzZnA5bjA2b2ZkNHYifQ.MWQzPqXVPqt3Kj0LxIWBdQ';
+// PRODUCTION
+//mapboxgl.accessToken = 'pk.eyJ1IjoiYWxjb3luZXh0IiwiYSI6ImNsanNiaHdhejA4eDMzZm4weGhqYTRnaHcifQ.37crDNKv4geComo0kCPQag';
 
-    var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/alcoynext/cljsfzfr9019401p596srfj11',
-        center: [-0.47298, 38.69826],
-        zoom: 13,
-        language: 'es'
-    });
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/alcoynext/cljsfzfr9019401p596srfj11',
+    center: [-0.47298, 38.69826],
+    zoom: 13,
+    language: 'es'
+});
 
-    map.addControl(new mapboxgl.NavigationControl());
-    map.addControl(new mapboxgl.ScaleControl());
+map.addControl(new mapboxgl.NavigationControl());
+map.addControl(new mapboxgl.ScaleControl());
 
-    map.on('load', () => {
-        
-    });
+map.on('load', () => {
 
-    map.on('idle', () => {
-        
-    });
-})
+});
+
+map.on('idle', () => {
+
+});
 
 /* Mapbox Directions */
-function route_tools(){
+function route_tools() {
     map.addControl(
         new MapboxDirections({
             accessToken: mapboxgl.accessToken,
@@ -40,7 +38,7 @@ function route_tools(){
 }
 
 /* Mapbox Geocoder */
-function search_tools(){
+function search_tools() {
     map.addControl(
         new MapboxGeocoder({
             accessToken: mapboxgl.accessToken,
